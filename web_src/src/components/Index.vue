a55513f1c4b7be1c16ec62aeed1159828b8df400
firzen <firzen13@gmail.com>
2019-08-21 23:09:04 +0800

    界面自定义调整

--- web_src/src/components/Index.vue
+++ web_src/src/components/Index.vue
@@ -15,12 +15,9 @@
 
           <div class="slide">
               <img src="static/logo/b_64.png" alt="">
-              <h2>{{$t("section_title1")}}</h2>
+              <h2>文档中心</h2>
               <p><span v-html="$t('section_description1')"></span></p>
-              <p>
-                  <a class="el-button " href="https://www.showdoc.cc/demo" target="_blank">{{$t("demo")}}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <a class="el-button" href="https://www.showdoc.cc/help" target="_blank" >{{$t("help")}}&nbsp;</i></a>
-              </p>
+              
           </div>
 
         </el-carousel-item>
@@ -28,21 +25,21 @@
         <el-carousel-item style="background-color: #2C606A;">
           <div class="slide">
               <h2>{{$t("section_title2")}}</h2>
-              <p><span v-html="$t('section_description2')"></span></p>
+             
           </div>
         </el-carousel-item>
 
         <el-carousel-item style="background-color: #f90;">
           <div class="slide">
               <h2>{{$t("section_title3")}}</h2>
-              <p><span v-html="$t('section_description3')"></span></p>
+              
           </div>
         </el-carousel-item>
 
         <el-carousel-item style="background-color: #7CBD9D;">
           <div class="slide">
               <h2>{{$t("section_title4")}}</h2>
-              <p><span v-html="$t('section_description4')"></span></p>
+              
           </div>
         </el-carousel-item>
 
@@ -53,29 +50,15 @@
           </div>
         </el-carousel-item>
 
-        <el-carousel-item style="background-color: #85CE92;">
-          <div class="slide">
-              <h2>{{$t("section_title6")}}</h2>
-              <p><span v-html="$t('section_description6')"></span></p>
-          </div>
-        </el-carousel-item>
+     
 
         <el-carousel-item style="background-color: #4BBFC3;">
           <div class="slide">
               <h2>{{$t("section_title7")}}</h2>
-              <p><span v-html="$t('section_description7')"></span></p>
+            
           </div>
         </el-carousel-item>
 
-        <el-carousel-item style="background-color: #1bbc9b;">
-          <div class="slide">
-              <h2></h2>
-              <p>{{$t("section_description8")}}</p>
-            <p>
-               <el-button><router-link to="/user/login">{{$t("section_title8")}}</router-link></el-button>
-            </p>
-          </div>
-        </el-carousel-item>
 
       </el-carousel>
     </div>
